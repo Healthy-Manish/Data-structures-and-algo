162ed00347b4305a645f3e0b3506008f67387390
Manish <manishdwivedi45165@gmail.com>
2023-07-03 14:26:40 +0530

    adding basics of java

---
+++ first.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class first{
+public static void main(String[] args) {
+    // output in java
+    System.out.print("Hello world");
+    //println adds new line after printing a line
+    System.out.println("My name is manish");
+
+
+    // input in java
+    Scanner input = new Scanner(System.in);
+    int n = input.nextInt();
+    System.out.println(n);
+
+}
+}
---
+++ primitive.java
@@ -0,0 +1,17 @@
+import javax.sound.sampled.AudioFileFormat.Type;
+
+import org.w3c.dom.TypeInfo;
+
+public class primitive {
+    public static void main(String[] args) {
+        // data types that can't be break further are primitive data types
+        int rollno = 64;
+        char letter = 'r';
+        float marks = 34.56f;
+        double largeDecimal = 23456.24566744;
+        long largeInteger = 3456745667884332457L;
+        boolean check = false;
+        System.out.println(check);
+
+    }
+}
