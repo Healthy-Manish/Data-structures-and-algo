449e747265414f7372a923fc0cc3a7d2ea1f94b6
Manish <manishdwivedi45165@gmail.com>
2023-07-19 14:37:02 +0530

    recursion in java

---
+++ recursion/Recursion1.java
@@ -0,0 +1,15 @@
+package recursion;
+
+public class Recursion1 {
+    public static void main(String[] args) {
+        print(1);
+    }
+    static void print(int n){
+        if (n==5) {
+        return;        }
+            System.out.println(n);
+
+            print(n+1);
+
+    }
+}
