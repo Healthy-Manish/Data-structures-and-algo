8894af8eecda286cb3fcf2fe0dbd3076e9a0058f
Manish <manishdwivedi45165@gmail.com>
2023-07-28 22:43:19 +0530

    extended the file

--- recursion/RecursionArray.java
+++ recursion/RecursionArray.java
@@ -6,6 +6,8 @@ public class RecursionArray {
         int[] arr = {1,2,3,5,6};
         System.out.println(sorted(arr, 0));
         System.out.println(linear(arr, 6,0));
+        int[] arr2 = {1,3,44,44,67,44};
+        linear2(arr2, 44, 0);
     }
     //checking whether array is sorted or not
     static boolean sorted(int[]arr, int index){
@@ -22,6 +24,15 @@ public class RecursionArray {
         }
         if (arr[index]==target)return true;
         return linear(arr, target, index+1);
+    }
+    static void linear2(int[]arr,int target, int index){
+        if (index==arr.length){
+            return ;
+        }
+        if (arr[index]==target){
+            System.out.println(index);
+        }
+         linear2(arr, target, index+1);
     }
 
 
