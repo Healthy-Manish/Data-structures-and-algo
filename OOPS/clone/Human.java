69b8b630e3e2b905269a2bd0c84d6fd15c03128e
healthy_manish <manishdwivedi45165@gmail.com>
2023-10-21 21:32:50 +0530

    shallow and deep copying

--- OOPS/clone/Human.java
+++ OOPS/clone/Human.java
@@ -3,16 +3,26 @@ package OOPS.clone;
 public class Human implements Cloneable {
     int age;
     String name;
+    int[] arr;
     public Human(int age,String name) {
         this.age = age;
         this.name = name;
+        this.arr = new int[]{3,4,5,6,7,89,9};
     }
     public Human(Human other){
         this.age = other.age;
         this.name = other.name;
     }
     public Object clone() throws CloneNotSupportedException{
-        return super.clone();
+       Human twin =  (Human)super.clone();//shallow copy
+
+//        making as deep copy
+        twin.arr = new int[twin.arr.length];
+        for (int i = 0; i <twin.arr.length ; i++) {
+            twin.arr[i]= this.arr[i];
+        }
+        return twin;
+
     }
 
 }
--- OOPS/clone/Main.java
+++ OOPS/clone/Main.java
@@ -1,5 +1,7 @@
 package OOPS.clone;
 
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) throws CloneNotSupportedException{
         Human Manish= new Human(34,"Manish");
@@ -7,6 +9,12 @@ public class Main {
             Human twin = (Human)Manish.clone();
         System.out.println(twin.age);
         System.out.println(twin.name);
+        System.out.println(Arrays.toString(twin.arr));
+
+        twin.arr[0]=100;
+        System.out.println(Arrays.toString(Manish.arr));
+        System.out.println(Arrays.toString(twin.arr));
+
     }
 
 
