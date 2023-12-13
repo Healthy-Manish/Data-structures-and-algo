e56f25f49ea5a0b44809a9d686add9ad6b553b21
healthy_manish <manishdwivedi45165@gmail.com>
2023-12-13 18:26:57 +0530

    balance binary tree

---
+++ Tree/Balanced_Binary_Tree.java
@@ -0,0 +1,17 @@
+package Tree;
+
+public class Balanced_Binary_Tree {
+    static boolean balance_binary(TreeNode root){
+    return dfsHeight(root)!=-1;
+    }
+    static int dfsHeight(TreeNode root){
+        if (root == null)return 0;
+        int leftHeight = dfsHeight(root.left);
+        if (leftHeight == -1)return -1;
+        int rightHeight = dfsHeight(root.right);
+        if (rightHeight==-1)return -1;
+
+        if (Math.abs(leftHeight-rightHeight)>1)return -1;
+        return Math.max(leftHeight,rightHeight)+1;
+    }
+}
