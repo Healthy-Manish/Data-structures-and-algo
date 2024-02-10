4910bf55dcdd4a4061481147a23ea033c034e541
healthy_manish <manishdwivedi45165@gmail.com>
2024-02-10 17:47:58 +0530

    find peak element in array

---
+++ Binary_Search/FIndPeakElement.java
@@ -0,0 +1,22 @@
+package Binary_Search;
+
+public class FIndPeakElement {
+
+//    search peak element in array
+    static int search(int[]arr){
+        int n =arr.length;
+        if (n==1)return arr[0];
+        if (arr[0]>arr[1])return arr[0];
+        if (arr[n-1]>arr[n-2])return arr[n-1];
+        int low = 1;
+        int high = n-2;
+        while (low<=high){
+            int mid = low+(high-low)/2;
+            if (arr[mid]>arr[mid-1] && arr[mid]>arr[mid+1])return arr[mid];
+            if (arr[mid]<arr[mid-1])high=mid-1;
+            else low=mid+1;
+        }
+        return -1;
+    }
+
+}
