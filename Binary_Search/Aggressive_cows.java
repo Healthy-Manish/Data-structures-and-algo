b35fb1b8cf03476d9a5215519c339a7d4d062762
healthy_manish <manishdwivedi45165@gmail.com>
2024-02-12 10:18:21 +0530

    aggresive cows using binary  search

---
+++ Binary_Search/Aggressive_cows.java
@@ -0,0 +1,36 @@
+package Binary_Search;
+
+import java.util.Arrays;
+
+public class Aggressive_cows {
+    static int agrresive(int[]arr,int cows){
+        Arrays.sort(arr);
+        int low = 1;
+        int high = arr[arr.length-1]-arr[0];
+        int ans = 0;
+        while(low<=high){
+            int mid = low+(high-low)/2;
+            if (CanWePlace(arr,cows,mid)){
+                ans= mid;
+                low = mid+1;
+            }
+            else{
+                high=mid-1;
+            }
+        }
+        return ans;
+    }
+    static boolean CanWePlace(int []arr, int cows,int dist){
+        int last = arr[0];
+        int cnt = 1;
+        for (int i = 1; i <arr.length ; i++) {
+            if (arr[i]-last>=dist){
+                cnt++;
+                last = arr[i];
+            }
+
+        }
+        if (cnt>=cows)return true;
+        return false;
+    }
+}
